62bd47ddd11a254b1cb40222061fb5622362a258
AliusGrigaliunas <Alius.grigaliunas@gmail.com>
2022-08-23 19:21:17 +0300

    feat(78):development and production environments implemented

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/main.ts',
-    mode: 'development',
     module: {
       rules: [
         {
@@ -19,19 +18,9 @@ module.exports = {
       filename: 'main.js',
       path: path.resolve(__dirname, 'public'),
     },
-    devServer: {
-        open: true,
-        static: {
-          directory: path.join(__dirname, 'public'),
-        },
-        compress: true,
-        port: 9000,
-      },
-
       plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.ejs',
-
             templateParameters: {
                 name: 'Aleksandras Gavėnas',
               },
